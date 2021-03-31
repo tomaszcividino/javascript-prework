d26d231c19ecfdb05b32e9e992c180d07ab4781b
Tomasz Cividino <tomaszcividino@gmail.com>
2021-03-31 21:31:13 +0200

    PapierZadanie

--- js/functions.js
+++ js/functions.js
@@ -5,4 +5,3 @@ function printMessage(msg) {
 }
 function clearMessages(){
     document.getElementById('messages').innerHTML=""
-}
---
+++ js/script.js
@@ -0,0 +1,7 @@
+let computerMove = 'kamień';
+
+printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = 'papier';
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nozyce, to wygrywasz!');
